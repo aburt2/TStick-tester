710175e3b76a826a6928072956b0cc14791c1b69
aburt2 <32888184+aburt2@users.noreply.github.com>
2023-08-02 12:59:59 -0400

    updated analysis script

--- wifi_deviation_analysis.py
+++ wifi_deviation_analysis.py
@@ -23,13 +23,18 @@ def analyse_data(poll_delay,board,parent_folder):
     filter_msg = msg[msg>0]
     filter_time = time[msg>0]
 
-    # # Print these out
-    # print(filter_msg)
-    # print(filter_time)
-
     # Get difference of time
     msgDif = filter_msg.diff()
-    logicDif = np.where(msgDif <= 0.02, True, False)
+    logicDif = np.where(msgDif > 0, 1, 0)
+    missedDif = np.where(np.logical_and(msgDif>0,msgDif<0.02),1,0)
+
+    # Calculate number of received messages, and missed messages
+    rxmsg = sum(logicDif)
+    missedmsg = len(msgDif)-sum(missedDif)
+    reliability = 1 - missedmsg/len(msgDif)
+
+    # Store in a dictionary
+    reliabilityStats = dict({'rxmsg':rxmsg, 'missedmsg':missedmsg, 'reliability':reliability})
 
     # Compute latency taking into account missed messages
     time_list = filter_time.to_list()
@@ -52,26 +57,24 @@ def analyse_data(poll_delay,board,parent_folder):
     timedf['SMA10'].dropna(inplace=True)
 
     # Get average and standard deviation
-    avg = tmpdf['Latency'].mean()
-    stdev = tmpdf['Latency'].std()
-    legendstr = 'Poll delay = ' + str(delay) +', Average = ' + str(np.round(avg,2)) + u"\u00B1" + str(np.round(stdev,2))
+    avg = timedf['Latency'].mean()
+    stdev = timedf['Latency'].std()
 
     # Get places without consecutive messages
     # timedf['consecutive'] = logicDif
 
     # Get array as a difference from the mean
     timedf['deviation'] = timedf['Latency'] - avg
+    timedf['std_deviation'] = timedf['deviation']/stdev
 
     # Create plot and axis labels
     fig, axs = plt.subplots(2)
 
     # Plot Time
     timedf[['Latency','SMA10']].plot(ax=axs[0],label='Latency', figsize=(16,8))
-    titlestr = 'Latency of ' + board + ', Poll delay = ' + str(poll_delay)
 
     # Plot Deviation
     timedf[['deviation']].plot(ax=axs[1],label='Deviation', figsize=(16,8))
-    titlestr = 'Latency of ' + board + ', Poll delay = ' + str(poll_delay)
 
     # Plot parameters
     # Latency Plot parameters
@@ -79,14 +82,34 @@ def analyse_data(poll_delay,board,parent_folder):
     # Deviation Plot parameters
     axs[1].set_ylabel('Deviation from Mean (ms)')
     # Figure parameters
+    titlestr = 'Latency of ' + board + ', Poll delay = ' + str(poll_delay) + ', Average = ' + str(np.round(avg,2)) + u"\u00B1" + str(np.round(stdev,2))
     fig.suptitle(titlestr)
 
     # Save figure
     fig.savefig(pic_fileName)
     plt.close()
 
+    # Print some statistics about this Data Set
+    # Set up strings
+    startstr = '\n************************************************'
+    endstr = '************************************************\n'
+    latencystr = 'Average Latency = ' + str(avg)
+    rxmsgstr = 'Received Messages = ' + str(rxmsg)
+    missedmsgstr = 'Missed Messages = ' + str(missedmsg)
+    reliabilitystr = 'Packet Loss = ' + str(np.round(100*(1-reliability),2)) +'%'
+    extremestr = 'Highest Latency = ' + str(max(timedf['Latency']))
+    loweststr = 'Lowest Latency = ' + str(min(timedf['Latency']))
+    print(startstr)
+    print(titlestr)
+    print(rxmsgstr)
+    print(missedmsgstr)
+    print(reliabilitystr)
+    print(extremestr)
+    print(loweststr)
+    print(endstr)
+
     # Return the df
-    return timedf
+    return timedf, reliabilityStats
 
     
 
@@ -97,24 +120,26 @@ parentFolder = 'selfResults'
 
 # Setup empty list
 dataDf = []
+relList = []
 legendList = []
 # Create axis object
 fig, ax = plt.subplots()
 xlim = 1e6
 fig.suptitle('Latency Deviation for TinyPico accross different delays')
-ax.set_ylabel('Deviation from Mean (ms)')
+ax.set_ylabel('Number of Standard Deviations from Mean')
 
 for delay in poll_delay:
     # Get time df from analysis
-    tmpdf = analyse_data(delay,board,parentFolder)
+    tmpdf,tmpreliability = analyse_data(delay,board,parentFolder)
     dataDf.append(tmpdf)
+    relList.append(tmpreliability)
 
     # Plto on axis
     avg = tmpdf['Latency'].mean()
     stdev = tmpdf['Latency'].std()
     legendstr = 'Poll delay = ' + str(delay) +', Average = ' + str(np.round(avg,2)) + u"\u00B1" + str(np.round(stdev,2))
     legendList.append(legendstr)
-    tmpdf[['deviation']].plot(ax=ax, label=legendstr, figsize=(16,8))
+    tmpdf[['std_deviation']].plot(ax=ax, label=legendstr, figsize=(16,8))
 
     # Get xlimit
     if len(tmpdf) < xlim:
